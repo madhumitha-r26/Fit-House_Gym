74993f92ceb88b8f9572307b0cf185ec07c8e9e1
madhumitha-ravichandran26 <madhumitha26301@gmail.com>
2024-08-29 15:40:16 +0530

    changed the popup forms

--- script.js
+++ script.js
@@ -7,68 +7,69 @@ navbarToggler.addEventListener('click', () => {
   navbarClose.classList.toggle('d-block');
 });
 
-var login_form= document.querySelector(".loginbox");
-var register_form= document.querySelector(".registerbox");
-var popupoverlay= document.querySelector(".overlay");
-var popupoverlay2= document.querySelector(".overlay2");
+// var login_form= document.querySelector(".loginbox");
+// var register_form= document.querySelector(".registerbox");
 
-var open_login= document.querySelector("#loginbtn");
-var open_register= document.querySelector("#registerbtn");
+// var open_login= document.querySelector("#loginbtn");
+// var open_register= document.querySelector("#registerbtn");
 
-var closelogin=document.querySelector("#close-loginform");
-var closeregister=document.querySelector("#close-registerform");
+// var closelogin=document.querySelector("#close-loginform");
+// var closeregister=document.querySelector("#close-registerform");
 
-var openfp=document.querySelector("#open-fpform");
-var closefp=document.querySelector("#close-fpform");
+// var openfp=document.querySelector("#open-fpform");
+// var closefp=document.querySelector("#close-fpform");
 
-var forgot_pwd =document.querySelector(".fpbox");
+// var forgot_pwd =document.querySelector(".fpbox");
 
-var username=document.getElementById("username");
-var email=document.getElementById("emailid");
-var phone=document.getElementById("phone");
-var pwd=document.getElementById("pwd");
-var session=document.getElementById("session");
-
-
-open_login.addEventListener("click",function()
-{
-  popupoverlay.style.display="block";
-  login_form.style.display="block";
-
-  email.value="";
-  pwd.value="";
-})
-
-open_register.addEventListener("click",function()
-{
-  popupoverlay.style.display="block";
-  register_form.style.display="block";
-
-  email.value="";
-  pwd.value="";
-  username.value="";
-  phone.value="";
-  session.value="Select your Session";
+// var username=document.getElementById("username");
+// var email=document.getElementById("emailid");
+// var phone=document.getElementById("phone");
+// var pwd=document.getElementById("pwd");
+// var pwd1=document.getElementById("pwd1");
+// var pwd2=document.getElementById("pwd2");
+// var session=document.getElementById("session");
 
 
-})
+// open_login.addEventListener("click",function()
+// {
+  
+//   login_form.style.display="block";
+//   email.value="";
+//   pwd.value="";
+// })
 
-closelogin.addEventListener("click",function(){
-  popupoverlay.style.display="none";
-  login_form.style.display="none";
-})
+// // open_register.addEventListener("click",function()
+// // {
+// //   register_form.style.display="block";
 
-closeregister.addEventListener("click",function(){
-  popupoverlay.style.display="none";
-  register_form.style.display="none";
-})
+// //   email.value="";
+// //   pwd.value="";
+// //   username.value="";
+// //   phone.value="";
+// //   session.value="Select your Session";
+// // })
 
-closefp.addEventListener("click",function(){
-  popupoverlay2.style.display="none";
-  forgot_pwd.style.display="none";
-})
+// closelogin.addEventListener("click",function(){
+//   login_form.style.display="none";
+// })
 
-openfp.addEventListener("click",function(){
-  popupoverlay2.style.display="block";
-  forgot_pwd.style.display="block";
-})
+// // closeregister.addEventListener("click",function(){
+// //   register_form.style.display="none";
+// // })
+
+// closefp.addEventListener("click",function(){
+//   forgot_pwd.style.display="none";
+//   login_form.style.display="block";
+
+//   email.value="";
+//   pwd.value="";
+// })
+
+// openfp.addEventListener("click",function(){
+//   forgot_pwd.style.display="block";
+//   login_form.style.display="none";
+
+//   email.value="";
+//   pwd1.value="";
+//   pwd2.value="";
+// })
